ae1c178497c22848bb3db45afe6c5523f727cdc4
daeeop <rlaeodjq681@gmail.com>
2025-05-17 12:00:24 +0900

    fix: build error

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '@repo/ui/styles/global.scss';
 import Header from '../src/layout/header';
 import styles from './style.module.scss';
+import '../src/styles/globals.scss';
 import TanstackQueryClientProvider from '../src/provider/queryClientProvider';
 
 export const metadata: Metadata = {
